a61f0ce75b252668b0147d5dd1e84bf4269eedc5
Sam Wu <swu@nait.ca>
2024-09-10 11:35:46 -0600

    added dataTable

--- dmit2015-firebase-faces-demo/src/main/java/dmit2015/faces/HelloView.java
+++ dmit2015-firebase-faces-demo/src/main/java/dmit2015/faces/HelloView.java
@@ -1,25 +1,39 @@
 package dmit2015.faces;
 
-import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 import org.omnifaces.util.Messages;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Named // create an instance of this class with same name with first letter in lowercase
         // helloView
-@RequestScoped
-public class HelloView {
+//@RequestScoped
+@ViewScoped
+public class HelloView implements Serializable {
 
     // Define a data field for storing username
+    @Getter @Setter
+    @NotBlank(message = "Please enter a username")
     private String username;
 
-    // Define getters/setts to encapsulate access to our data fields
-    public String getUsername() {
-        return username;
-    }
+    // Define a list of String username
+    @Getter
+    private List<String> usernameList = new ArrayList<>();
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    // Define getters/setts to encapsulate access to our data fields
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
 
     // Define a method to handle a submit action
     public void onSubmit() {
@@ -29,6 +43,18 @@ public class HelloView {
         // Send an info message to FacesContext with a greeting message
         Messages.addGlobalError("Welcome {0} to Jakarta Faces error message",
                 username);
+        // Clear the username field
+        username = null;
+    }
 
+    // Define a method to add username to usernameList
+    public void onAddUsername() {
+        usernameList.add(username);
+    }
+
+    // Define a method to clear username and usernameList
+    public void onClear() {
+        usernameList.clear();
+        username = null;
     }
 }
